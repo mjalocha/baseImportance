5e20232486e8e32b72c187c0fa160b50eb5a44a6
Mateusz Jalocha <mjalocha@pl-wro-4c4fmh2.ULTIMO>
2021-06-10 14:36:57 +0200

    baseImpact weights tests added

--- tests/testthat/test-baseImpact_combined.R
+++ tests/testthat/test-baseImpact_combined.R
@@ -4,3 +4,9 @@ test_that("Base Impact Combined", {
   expect_equal(baseImpact_combined(df2, c(102,103), 104)$impact, c(0.95104895, 0.80769231, 0.39370630, 0.03269231))
   expect_equal(baseImpact_combined(df3, c(102,103), 104)$impact,c(0.94754635, 0.60967015, 0.69685315, 0.01634616))
 })
+
+test_that("Base Impact Combined weights", {
+  df3 = "./data/df3.xlsx"
+  expect_equal(baseImpact_combined(df3, c(102,103), 104, weights_models = c(0.8,0.2))$impact %>% round(8),
+               c(0.95561429, 0.51784516, 0.57874126, 0.02615385))
+})
